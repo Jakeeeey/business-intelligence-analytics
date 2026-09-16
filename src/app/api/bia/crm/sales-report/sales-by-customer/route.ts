e6b1751baa9c2486dbb5c7df843af86d0cdda1d5
Erman <66209038+Xerman09@users.noreply.github.com>
2026-09-16 21:15:01 +0800

    fix ci

--- src/app/api/bia/crm/sales-report/sales-by-customer/route.ts
+++ src/app/api/bia/crm/sales-report/sales-by-customer/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import process from "node:process";
 
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
